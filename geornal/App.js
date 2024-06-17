59110e2dc79764930825a64517a0a3feef82dfd2
Maria Fe Fortes <mfortes@up.edu.ph>
2024-06-17 14:13:04 +0800

    added styles

--- geornal/App.js
+++ geornal/App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 import Welcome from './screens/Welcome';
 import CameraScreen from './screens/Camera';
@@ -17,8 +18,62 @@ const App = () => {
         <Stack.Screen name="Camera" component={CameraScreen} />
         <Stack.Screen name="Entry" component={Entry} />
       </Stack.Navigator>
+
+      <View style = {styles.container}>
+        <View style = {styles.box}>
+          <Text style = {styles.heading}> Camera </Text>
+          <TouchableOpacity style = {styles.button}>
+            <Text style = {styles.buttonText}> Capture</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style = {styles.box}>
+          <Text style = {styles.heading}> Text Input</Text>
+          <TextInput
+            style = {styles.textInput}
+            placeholder = "Insert description here..."
+            multiline = {true}
+            numberOfLines= {6}
+          />
+        </View>
+      </View>
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+    backgroundColor: '#ffffff'
+  },
+  box: {
+    backgroundColor: '#990000',
+    width: '100%',
+    height: '30%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  heading: {
+    fontWeight: 'light',
+    fontSize: '20',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#009933'
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#ffffff'
+  },
+  textInput: {
+    height: '40%',
+    width: '60%',
+    color: '#e6e6e6'
+  },
+});
 
 export default App;
