2e67e60402e1766c11cbbbe141ab092b82e378f7
Justin Sta. Ana <jmstaana6@up.edu.ph>
2024-06-21 09:45:10 +0800

    Final Project Submission

--- geornal/screens/Welcome.js
+++ geornal/screens/Welcome.js
@@ -1,44 +1,46 @@
+// screens/Welcome.js
 import React, { useEffect, useState } from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Button, Text, StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Welcome = ({ navigation }) => {
-  const initialLatitude = 14.656363;
-  const initialLongitude = 121.069664;
-
+const Welcome= ({ navigation }) => {
   const [currentLocation, setCurrentLocation] = useState({
     coords: {
-      latitude: initialLatitude,
-      longitude: initialLongitude,
+      latitude: 14.656363,
+      longitude: 121.069664,
     },
   });
-  const [markers, setMarkers] = useState([]);
+  const [markers, setMarkers] = useState([])
 
   useEffect(() => {
-    const fetchLocationAndMarkers = async () => {
+    (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         console.error('Permission to access location was denied');
         return;
       }
 
-      try {
-        let location = await Location.getCurrentPositionAsync({});
-        setCurrentLocation(location);
+      let location = await Location.getCurrentPositionAsync({});
+      setCurrentLocation(location);
 
+      try {
         const jsonValue = await AsyncStorage.getItem('entries');
-        if (jsonValue !== null) {
-          setMarkers(JSON.parse(jsonValue));
+        if (jsonValue != null) {
+          setMarkers(JSON.parse(jsonValue))
         }
       } catch (e) {
-        console.error('Error fetching location and markers:', e);
+        // error reading value
       }
-    };
-
-    fetchLocationAndMarkers();
+    })();
   }, []);
+
+  const handleOpenCameraButtonPress = () => {
+    setShowCamera(!showCamera);
+  };
+
+  console.log('markers', markers)
 
   return (
     <View style={{ flex: 1 }}>
@@ -52,50 +54,41 @@ const Welcome = ({ navigation }) => {
         }}
       >
         <Marker
-          coordinate={{
-            latitude: currentLocation.coords.latitude,
-            longitude: currentLocation.coords.longitude,
-          }}
+          coordinate={{ latitude: currentLocation.coords.latitude, longitude: currentLocation.coords.longitude }}
           pinColor="#52685b"
-          title="Current Location"
+          title="Current Location" 
         />
-        {markers.length > 0 &&
-          markers.map((marker, index) => (
-            <Marker
-              key={index}
-              coordinate={{
-                latitude: marker.coords.latitude,
-                longitude: marker.coords.longitude,
-              }}
-              title={marker.title}
-            />
-          ))}
+        {markers.length > 0 && markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: marker.currentLocation.coords.latitude, longitude: marker.currentLocation.coords.longitude }}
+            title={marker.title}
+          />
+          ))
+        }
       </MapView>
 
       <TouchableOpacity
         style={{
-          position: 'absolute',
-          bottom: 20,
-          right: 20,
+          opacity:100,
+          marginVertical: 5,
+          marginHorizontal:160,
           borderRadius: 100,
-          backgroundColor: '#e0e497',
-          height: 75,
+          backgroundColor: '#e0e497', 
+          height:75,
           width: 75,
-          justifyContent: 'center',
-          alignItems: 'center',
+          justifyContent: 'center',  
+          alignItems: 'center', 
         }}
-        onPress={() => navigation.navigate('Camera', { currentLocation })}
+        onPress={()=>navigation.navigate("Camera", { currentLocation })}
       >
-        <Text
-          style={{
-            color: '#52685b',
-            fontFamily: 'Impact',
-            fontSize: 15,
-            textAlign: 'center',
-          }}
-        >
-          Open Camera
-        </Text>
+        <Text style={{ 
+          color: '#52685b', 
+          fontFamily: 'Impact',
+          fontSize: 15,
+          textAlign: 'center'
+
+           }}>Open Camera</Text>
       </TouchableOpacity>
     </View>
   );
